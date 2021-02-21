2b980c47102b434ec470bdb891e7d7c8f4ee5526
bkoplin <benjaminkoplin@gmail.com>
2021-02-21 08:02:56 -0600

    Add functionality to upload query objects (if present)

--- types.d.ts
+++ types.d.ts
@@ -3,7 +3,13 @@ declare module "*.vue" {
     export default Vue;
 }
 
-export interface PageDataObject {
+interface UserSaveObject {
+    createDate?: string;
+    createByUrn?: string;
+    lastEditByUrn: string;
+    lastEditDate: string;
+}
+export type PageDataObject = UserSaveObject & {
     urn: string;
     names: string[];
     name: string;
@@ -17,17 +23,13 @@ export interface PageDataObject {
     entityTypeUrns: string[];
     combinedItemUrns: string[];
     inactive: boolean;
-    createDate?: string;
-    createByUrn?: string;
-    lastEditByUrn: string;
-    lastEditDate: string;
     deprecated?: Boolean;
     lastEditDateObs?: string;
     lastEditByUrnObs?: string;
     textSet?: string[];
     test?: string;
     pages: Page[];
-}
+} 
 
 export interface OperationImpl {
     operation: string;
@@ -50,4 +52,35 @@ export interface ServiceOutput {
     outputContentTypeUrn: string;
     oldContentType: string;
     maxAge: string;
+}
+
+export type QueryDataObject = UserSaveObject & {
+    urn: string;
+    name: string;
+    summary: string;
+    queryString: string;
+    securityFunctionUrns: string[];
+    boundToEntityTypeUrn?: string;
+    childQueries?: ChildQuery[];
+    inactive: boolean;
+    queryEngineUrn?: string;
+    dataSpaceUrn?: string;
+    queryParams?: QueryParam[];
+}
+
+export interface ChildQuery {
+    urn: string;
+    queryString: string;
+    dataSpaceUrn: string;
+    queryParams?: QueryParam[];
+}
+
+export interface QueryParam {
+    codeName: string;
+    valueTypeUrn:
+        | "urn:com:loki:core:model:types:string"
+        | "urn:com:loki:core:model:types:bool"
+        | "urn:com:loki:core:model:types:integer"
+        | "urn:com:loki:core:model:types:date"
+        | "urn:com:loki:core:model:types:decimal";
 }
