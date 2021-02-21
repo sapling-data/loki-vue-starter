5bd14dee53f2e55acc0dc3466385550a7a5f9347
bkoplin <benjaminkoplin@gmail.com>
2021-02-21 06:50:13 -0600

    add querydataobject types

--- types.d.ts
+++ types.d.ts
@@ -51,3 +51,38 @@ export interface ServiceOutput {
     oldContentType: string;
     maxAge: string;
 }
+
+export interface QueryDataObject {
+    urn: string;
+    name: string;
+    summary: string;
+    queryString: string;
+    securityFunctionUrns: string[];
+    boundToEntityTypeUrn?: string;
+    childQueries: ChildQuery[];
+    inactive: boolean;
+    createDate: string;
+    createByUrn: string;
+    lastEditDate: string;
+    lastEditByUrn: string;
+    queryEngineUrn: string;
+    dataSpaceUrn: string;
+    queryParams: QueryParam[];
+}
+
+export interface ChildQuery {
+    urn: string;
+    queryString: string;
+    dataSpaceUrn: string;
+    queryParams?: QueryParam[];
+}
+
+export interface QueryParam {
+    codeName: string;
+    valueTypeUrn:
+        | "urn:com:loki:core:model:types:string"
+        | "urn:com:loki:core:model:types:bool"
+        | "urn:com:loki:core:model:types:integer"
+        | "urn:com:loki:core:model:types:date"
+        | "urn:com:loki:core:model:types:decimal";
+}
