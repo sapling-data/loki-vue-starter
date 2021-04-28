94bb1d71845ab350ea6d9c373cccf365adfee99b
Andrew Waller <48367637+andrewhwaller@users.noreply.github.com>
2021-04-28 17:11:54 -0500

    Revert "Create Vite Plugin to appropriately reference Loki resource files"
    This reverts commit b3a32d218764424db1b5cc6c3c8780cb338018d1.

--- uploadToLoki.js
+++ uploadToLoki.js
@@ -1,112 +1,53 @@
-// @ts-check
-/* eslint-disable no-console, no-loop-func */
+/* eslint-disable no-console */
+/* eslint-disable no-loop-func */
 require('dotenv').config();
 
 const axios = require('axios').default;
 const fs = require('fs');
 const packageJson = require('./package.json');
 
-const {
-  appInfo: { loki },
-} = packageJson;
-const baseUrl = `https://${loki.cloudPrefix}.saplingdata.com/${loki.appCodeName}-AppBuilder/api`;
-const resourceUrl = '/urn/com/loki/core/model/api/resource/v';
-const pageFileListUrl = `/urn/com/loki/core/model/api/list/v/urn/com/${loki.cloudCodeName}/${loki.appCodeName}/app/pages/${loki.pageCodeName}?format=json`;
-const pageFileUploadUrl = `/urn/com/loki/core/model/api/resource/v/urn/com/${loki.cloudCodeName}/${loki.appCodeName}/app/pages/${loki.pageCodeName}!`;
-const pageDataUploadUrl = `/urn/com/loki/modeler/model/types/combinedPageExt/v/urn/com/${loki.cloudPrefix}/${loki.appCodeName}/app/pages/${loki.pageCodeName}`;
-
-const lokiSession = axios.create({
-  baseURL: baseUrl,
-  auth: {
-    username: process.env.LOKI_USERNAME,
-    password: process.env.LOKI_PASSWORD,
-  },
-});
-
-const pageDataObject = {
-  urn: `urn:com:${loki.cloudPrefix}:${loki.appCodeName}:app:pages:${loki.pageCodeName}`,
-  names: [loki.pageName],
-  name: loki.pageName,
-  summary: '',
-  description: null,
-  descriptionHtml: null,
-  serviceOutput: {
-    outputContentTypeUrn: 'urn:com:loki:meta:data:mediaTypes:text%2Fhtml',
-    oldContentType: 'urn:com:loki:meta:data:mediaTypes:text%2Fhtml',
-    maxAge: '0',
-  },
-  operationImpls: [
-    {
-      operation: 'urn:com:loki:core:model:operations:webService',
-      method:
-                'urn:com:loki:core:model:operations:webService:methods:freemarkerPage',
-      pageTemplate: `urn:com:${loki.cloudPrefix}:${loki.appCodeName}:app:pages:${loki.pageCodeName}!index.html`,
-      securityFunctionGroups: [],
-      actionImpls: [
-        {
-          action: 'urn:com:loki:core:model:actions:get',
-          securityFunctionGroups: [
-            `urn:com:${loki.cloudPrefix}:${loki.appCodeName}:model:functions:generalAccess`,
-          ],
-        },
-      ],
-    },
-    {
-      operation: 'urn:com:loki:core:model:operations:render',
-      method: 'urn:com:loki:freemarker:model:methods:freemarkerRender',
-      pageTemplate: `urn:com:${loki.cloudPrefix}:${loki.appCodeName}:app:pages:${loki.pageCodeName}!index.html`,
-      securityFunctionGroups: [],
-      actionImpls: [],
-    },
-  ],
-  purposeUrns: ['urn:com:loki:core:data:servicePurposeSet#page'],
-  boundToEntityTypeUrn: null,
-  entityTypeUrns: [
-    'urn:com:loki:meta:model:types:webPage',
-    'urn:com:loki:meta:model:types:service',
-  ],
-  combinedItemUrns: [],
-  inactive: false,
-  lastEditByUrn: process.env.LOKI_USER_URN,
-  lastEditDate: new Date().toISOString(),
-  pages: [
-    {
-      urn: `urn:com:${loki.cloudPrefix}:${loki.appCodeName}:app:pages:${loki.pageCodeName}!index.html`,
-    },
-  ],
-};
+const baseUrl = `https://${packageJson.appInfo.loki.cloudPrefix}.saplingdata.com/${packageJson.appInfo.loki.appName}-AppBuilder/api`;
+const resourceUrl = `${baseUrl}/urn/com/loki/core/model/api/resource/v`;
+const pageFileListUrl = `${baseUrl}/urn/com/loki/core/model/api/list/v/urn/com/${packageJson.appInfo.loki.cloudName}/${packageJson.appInfo.loki.appName}/app/pages/${packageJson.appInfo.loki.pageName}?format=json`;
+const pageFileUploadUrl = `${baseUrl}/urn/com/loki/core/model/api/resource/v/urn/com/${packageJson.appInfo.loki.cloudName}/${packageJson.appInfo.loki.appName}/app/pages/${packageJson.appInfo.loki.pageName}!`;
 
 const pushToLoki = async () => {
   const distFiles = fs.readdirSync('./dist');
 
-  await lokiSession.post(pageDataUploadUrl, pageDataObject).then(() => {
-    console.log(
-      `Loki data for "${pageDataObject.name}" saved to ${pageDataObject.urn} (${pageDataUploadUrl})`,
-    );
-  });
   distFiles.forEach((file) => {
     const filePath = `./dist/${file}`;
 
     fs.readFile(filePath, 'utf8', (err, data) => {
-      const baseFileName = file;
-      // const baseFileName = file.replace(`${loki.pageCodeName}!`, '');
-      const uploadUrl = pageFileUploadUrl + baseFileName;
-      lokiSession
-        .post(uploadUrl, data, {
+      const uploadUrl = pageFileUploadUrl + file;
+      axios.post(
+        uploadUrl,
+        data,
+        {
           headers: {
             'Content-Type': 'text/plain',
           },
-        })
-        .then(() => {
-          console.log(`${file} uploaded to ${uploadUrl}`);
-        });
+          auth: {
+            username: process.env.LOKI_USERNAME,
+            password: process.env.LOKI_PASSWORD,
+          },
+        },
+      ).then(() => {
+        console.log(`${file} uploaded to ${uploadUrl}`);
+      });
     });
   });
 };
 
 function getCurrentFiles() {
-  return lokiSession
-    .get(pageFileListUrl)
+  return axios({
+    baseUrl,
+    method: 'GET',
+    auth: {
+      username: process.env.LOKI_USERNAME,
+      password: process.env.LOKI_PASSWORD,
+    },
+    url: pageFileListUrl,
+  })
     .then((response) => response.data.results)
     .catch((error) => {
       console.error(error.response.data.errors);
@@ -114,19 +55,23 @@ function getCurrentFiles() {
 }
 
 async function deleteCurrentFiles(files) {
-  console.log(
-    `\x1b[34mDeleting ${files.length} files from the page...\x1b[89m`,
-  );
+  console.log(`\x1b[34mDeleting ${files.length} files from the page...\x1b[89m`);
 
   // eslint-disable-next-line no-plusplus
   for (let i = 0; i < files.length; i++) {
     const deleteUrl = `${resourceUrl}/${files[i].urn.replace(/[:]/g, '/')}`;
     // eslint-disable-next-line no-await-in-loop
-    await lokiSession
-      .delete(deleteUrl)
-      .then((d) => {
-        console.log(`\x1b[32m${files[i].urn} was deleted!\x1b[32m`);
-      })
+    await axios({
+      baseUrl,
+      method: 'DELETE',
+      auth: {
+        username: process.env.LOKI_USERNAME,
+        password: process.env.LOKI_PASSWORD,
+      },
+      url: deleteUrl,
+    }).then(() => {
+      console.log(`\x1b[32m${files[i].urn} was deleted!\x1b[32m`);
+    })
       .catch((error) => {
         console.log(`There was a problem deleting ${files[i].urn}...`);
         console.error(error.response.data.errors);
