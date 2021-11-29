422ae8fb94fe297a7909a0c1fa35a9fecf17c2b8
Anthony Bui <anthony.bui@saplingdata.com>
2021-11-29 16:43:46 -0600

    feat: Added common files to .gitignore, added .env.example file

--- jest.unit.config.js
+++ jest.unit.config.js
@@ -1,21 +1,10 @@
 module.exports = {
-  moduleFileExtensions: [
-    'js',
-    'ts',
-    'json',
-    'vue',
-  ],
-  transform: {
-    '^.+\\.js$': 'babel-jest',
-    '^.+\\.vue$': 'vue-jest',
-  },
-  collectCoverage: true,
-  collectCoverageFrom: [
-    'src/components/*.{js,vue}',
-    '!**/node_modules/**',
-  ],
-  coverageReporters: [
-    'html',
-    'text-summary',
-  ],
+	moduleFileExtensions: ['js', 'ts', 'json', 'vue'],
+	transform: {
+		'^.+\\.js$': 'babel-jest',
+		'^.+\\.vue$': 'vue-jest',
+	},
+	collectCoverage: true,
+	collectCoverageFrom: ['src/components/*.{js,vue}', '!**/node_modules/**'],
+	coverageReporters: ['html', 'text-summary'],
 };
