d6aaea4d6d8fcee0c7a9ab00bbc4011056256369
Andrew Waller <andrew.waller@saplingdata.com>
2022-01-21 20:19:44 -0600

    Add custom plugins and import loki.config.mjs; add additional entry point for html

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,32 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import injectLokiFreemarkerPlugin from './plugins/injectLokiFreemarker';
+import injectDevMainPlugin from './plugins/injectDevMain';
+import { injectHtml } from 'vite-plugin-html';
+// eslint-disable-next-line import/extensions
+import lokiConfig from './loki.config.mjs';
 
 const { resolve } = require('path');
 
-const packageJson = require('./package.json');
-
 export default defineConfig({
   root: './',
-  base: process.env.NODE_ENV === 'production' ? `/${packageJson.appInfo.loki.appName}/api/urn/com/loki/core/model/api/modelResource/v/urn/com/${packageJson.appInfo.loki.cloudName}/${packageJson.appInfo.loki.appName}/app/pages/${packageJson.appInfo.loki.pageName}/` : '/',
-  plugins: [vue()],
+  base: process.env.NODE_ENV === 'production' ? `/${lokiConfig.appName}/api/urn/com/loki/core/model/api/modelResource/v/urn/com/${lokiConfig.cloudName}/${lokiConfig.appName}/app/pages/${lokiConfig.pageName}/` : '/',
+  plugins: [
+    vue(),
+    injectHtml({
+      enforce: 'pre',
+      data: {
+        title: 'Title',
+      },
+    }),
+    injectDevMainPlugin(),
+    injectLokiFreemarkerPlugin(),
+  ],
   build: {
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'index.html'),
+        main: resolve(__dirname, 'src/main.js'),
+        html: resolve(__dirname, 'index.html'),
       },
     },
     outDir: 'dist',
