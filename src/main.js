f209a25d422a0fe45889f17dbf77d85b554da65a
Andrew Waller <67027415+andrew-waller@users.noreply.github.com>
2022-01-22 11:05:09 -0600

    v1.0.1 (#21)
    * Remove extra plugin
    
    * Remove ejs rendering and filesystem call
    
    * Remove ejs rendering; reformat jQuery/loki tag objects; add empty flag and filter for empty html chunk
    
    * Remove injectHtml plugin and change input order
    
    * Update gitignore
    
    * Add comments and remove redundant addConnection calls/variable declarations

--- src/main.js
+++ src/main.js
@@ -6,81 +6,56 @@ import { createPinia } from 'pinia';
 import Loki from '@sapling-data/loki-javascript-client/dist/es-bundle';
 import App from './App.vue';
 import router from './router/router';
+// eslint-disable-next-line import/extensions
 import lokiConfig from '../loki.config.mjs';
 
+// If you are in development mode, you will need to create/authorize a new Loki instance. If your
+// code is being served in production mode from a Sapling server, this step is not necessary and
+// will be handled by lokiSession and the other libraries that are added on build.
 if (import.meta.env.MODE === 'development') {
-  lokiConfig.baseUrl = `https://${lokiConfig.cloudPrefix}.saplingdata.com`;
   lokiConfig.auth = {
     username: import.meta.env.VITE_LOKI_USERNAME,
     password: import.meta.env.VITE_LOKI_PASSWORD,
   };
-
   window.loki = new Loki(lokiConfig);
-  const rootUrn = `urn:com:${lokiConfig.cloudName}:cloudControl`;
-  const keyUrn = null;
-  const cloudUrlBase = `${lokiConfig.baseUrl}/${lokiConfig.internal ? 'sd-cloud' : 'cloud'}`;
-  window.loki.environ.addConnection({
-    urn: `urn:com:${lokiConfig.cloudName}:registry:cfg:connections:cloudControl-dev`,
-    url: `${cloudUrlBase}/api`,
-    rootUrn,
-    serviceGroupUrns: ['urn:com:saplingdata:cloudControl:model:serviceGroups:userServices', 'urn:com:saplingdata:cloudControl:model:serviceGroups:cloudServices'],
-    keyUrn,
-  });
-  window.loki.environ.addConnection({
-    urn: `urn:com:${lokiConfig.cloudName}:registry:cfg:connections:cloudControl-ui-dev`,
-    url: `${cloudUrlBase}/pages`,
-    rootUrn,
-    serviceGroupUrns: ['urn:com:saplingdata:cloudControl:model:serviceGroups:userServices-ui', 'urn:com:saplingdata:cloudControl:model:serviceGroups:cloudServices-ui'],
-    keyUrn,
-  });
-  window.loki.environ.addConnection({
-    urn: `urn:com:${lokiConfig.cloudName}:registry:cfg:connections:cloudControl-dev`,
-    url: `${cloudUrlBase}/api`,
-    rootUrn,
-    serviceGroupUrns: ['urn:com:saplingdata:cloudControl:model:serviceGroups:userServices', 'urn:com:saplingdata:cloudControl:model:serviceGroups:cloudServices'],
-    keyUrn,
-  });
-  window.loki.environ.addConnection({
-    urn: `urn:com:${lokiConfig.cloudName}:registry:cfg:connections:cloudControl-ui-dev`,
-    url: `${cloudUrlBase}/pages`,
-    rootUrn,
-    serviceGroupUrns: ['urn:com:saplingdata:cloudControl:model:serviceGroups:userServices-ui', 'urn:com:saplingdata:cloudControl:model:serviceGroups:cloudServices-ui'],
-    keyUrn,
-  });
-} else {
-  lokiConfig.baseUrl = `https://${lokiConfig.cloudPrefix}.saplingdata.com`;
-  const keyUrn = null;
-  const rootUrn = `urn:com:${lokiConfig.cloudName}:cloudControl`;
-  const cloudUrlBase = `${lokiConfig.baseUrl}/${lokiConfig.internal ? 'sd-cloud' : 'cloud'}`;
-  loki.environ.addConnection({
-    urn: `urn:com:${lokiConfig.cloudName}:registry:cfg:connections:cloudControl-dev`,
-    url: `${cloudUrlBase}/api`,
-    rootUrn,
-    serviceGroupUrns: ['urn:com:saplingdata:cloudControl:model:serviceGroups:userServices', 'urn:com:saplingdata:cloudControl:model:serviceGroups:cloudServices'],
-    keyUrn,
-  });
-  loki.environ.addConnection({
-    urn: `urn:com:${lokiConfig.cloudName}:registry:cfg:connections:cloudControl-ui-dev`,
-    url: `${cloudUrlBase}/pages`,
-    rootUrn,
-    serviceGroupUrns: ['urn:com:saplingdata:cloudControl:model:serviceGroups:userServices-ui', 'urn:com:saplingdata:cloudControl:model:serviceGroups:cloudServices-ui'],
-    keyUrn,
-  });
-  loki.environ.addConnection({
-    urn: `urn:com:${lokiConfig.cloudName}:registry:cfg:connections:cloudControl-dev`,
-    url: `${cloudUrlBase}/api`,
-    rootUrn,
-    serviceGroupUrns: ['urn:com:saplingdata:cloudControl:model:serviceGroups:userServices', 'urn:com:saplingdata:cloudControl:model:serviceGroups:cloudServices'],
-    keyUrn,
-  });
-  loki.environ.addConnection({
-    urn: `urn:com:${lokiConfig.cloudName}:registry:cfg:connections:cloudControl-ui-dev`,
-    url: `${cloudUrlBase}/pages`,
-    rootUrn,
-    serviceGroupUrns: ['urn:com:saplingdata:cloudControl:model:serviceGroups:userServices-ui', 'urn:com:saplingdata:cloudControl:model:serviceGroups:cloudServices-ui'],
-    keyUrn,
-  });
 }
+
+// The following code initializes connections for the CloudMenu component. Note that connections
+// are added to the Loki instance and only need to be added on initialization. Since this is a
+// global setup/initialization task, the responsibility for creating connections lies here
+// as opposed to, for example, the setup function for the CloudMenu component itself.
+const keyUrn = null;
+const rootUrn = `urn:com:${lokiConfig.cloudName}:cloudControl`;
+const cloudUrlBase = `https://${lokiConfig.cloudPrefix}.saplingdata.com/${lokiConfig.internal ? 'sd-cloud' : 'cloud'}`;
+
+loki.environ.addConnection({
+  urn: `urn:com:${lokiConfig.cloudName}:registry:cfg:connections:cloudControl-dev`,
+  url: `${cloudUrlBase}/api`,
+  rootUrn,
+  serviceGroupUrns: ['urn:com:saplingdata:cloudControl:model:serviceGroups:userServices', 'urn:com:saplingdata:cloudControl:model:serviceGroups:cloudServices'],
+  keyUrn,
+});
+loki.environ.addConnection({
+  urn: `urn:com:${lokiConfig.cloudName}:registry:cfg:connections:cloudControl-ui-dev`,
+  url: `${cloudUrlBase}/pages`,
+  rootUrn,
+  serviceGroupUrns: ['urn:com:saplingdata:cloudControl:model:serviceGroups:userServices-ui', 'urn:com:saplingdata:cloudControl:model:serviceGroups:cloudServices-ui'],
+  keyUrn,
+});
+loki.environ.addConnection({
+  urn: `urn:com:${lokiConfig.cloudName}:registry:cfg:connections:cloudControl-dev`,
+  url: `${cloudUrlBase}/api`,
+  rootUrn,
+  serviceGroupUrns: ['urn:com:saplingdata:cloudControl:model:serviceGroups:userServices', 'urn:com:saplingdata:cloudControl:model:serviceGroups:cloudServices'],
+  keyUrn,
+});
+loki.environ.addConnection({
+  urn: `urn:com:${lokiConfig.cloudName}:registry:cfg:connections:cloudControl-ui-dev`,
+  url: `${cloudUrlBase}/pages`,
+  rootUrn,
+  serviceGroupUrns: ['urn:com:saplingdata:cloudControl:model:serviceGroups:userServices-ui', 'urn:com:saplingdata:cloudControl:model:serviceGroups:cloudServices-ui'],
+  keyUrn,
+});
 
 const app = createApp(App);
 app.use(createPinia());
