d3023d79b147669048173e558f85f14e007f5500
Andrew Waller <andrew.waller@saplingdata.com>
2022-01-21 23:25:11 -0600

    Remove injectHtml plugin and change input order

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import injectLokiFreemarkerPlugin from './plugins/injectLokiFreemarker';
 import injectDevMainPlugin from './plugins/injectDevMain';
-import { injectHtml } from 'vite-plugin-html';
 // eslint-disable-next-line import/extensions
 import lokiConfig from './loki.config.mjs';
 
@@ -13,20 +12,14 @@ export default defineConfig({
   base: process.env.NODE_ENV === 'production' ? `/${lokiConfig.appName}/api/urn/com/loki/core/model/api/modelResource/v/urn/com/${lokiConfig.cloudName}/${lokiConfig.appName}/app/pages/${lokiConfig.pageName}/` : '/',
   plugins: [
     vue(),
-    injectHtml({
-      enforce: 'pre',
-      data: {
-        title: 'Title',
-      },
-    }),
     injectDevMainPlugin(),
     injectLokiFreemarkerPlugin(),
   ],
   build: {
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'src/main.js'),
         html: resolve(__dirname, 'index.html'),
+        main: resolve(__dirname, 'src/main.js'),
       },
     },
     outDir: 'dist',
